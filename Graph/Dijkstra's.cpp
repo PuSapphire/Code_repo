5461562fe6e4b66febc6bba18ba12955ecb96f20
PuSapphire <85485902+PuSapphire@users.noreply.github.com>
2021-09-24 20:33:55 +0800

    Update Dijkstra's.cpp

--- Graph/Dijkstra's.cpp
+++ Graph/Dijkstra's.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 const int SZ = 1e5;
-vector<ll> dis(SZ, 9e18);
-vector<vector<pll>> adj(SZ);
+vector<int> dis(SZ, 2e9);
+vector<vector<pair<int, int>>> adj(SZ);
 dis[s] = 0;
 
 void dijkstra() {
   //pair<vtx id, dist>
-  priority_queue<pll, vector<pll>, greater<pll>> pq;
+  priority_queue<int, vector<int>, greater<int>> pq;
   pq.push({0, s});
   while (pq.size()) {
-    ll now = pq.top().ft;
-    ll d = pq.top().sd;
+    int now = pq.top().ft;
+    int d = pq.top().sd;
     pq.pop();
     if (t[now]) continue;
     t[now] = 1;
 
-    for (pll &vtx : adj[now]) {
+    for (pair<int, int> &vtx : adj[now]) {
       if (t[vtx.ft]) continue;
       if (d+vtx.sd < dis[vtx.ft]) {
         dis[vtx.ft] = d+vtx.sd;
