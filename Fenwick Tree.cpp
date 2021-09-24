a6811df93ca2d7f8dfe0bceac20a95937c7d1ed5
PuSapphire <85485902+PuSapphire@users.noreply.github.com>
2021-09-24 20:20:12 +0800

    Create Fenwick Tree.cpp

---
+++ Fenwick Tree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int SZ = 100;
+int bit[SZ+1]
+
+void update (int i, int v) {
+  for (; i <= SZ; i += i&-i)
+    bit[u] += v;
+};
+
+int query (int i) {
+  int s = 0;
+  for (; i > 0; i -= i&-i)
+    s += bit[i];
+  return s;
+};
